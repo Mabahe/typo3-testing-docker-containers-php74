d73518036c7ad3f5a7e50d6a3f769efd1f450bee
Marc Bastian Heinrichs <typo3@mbh-software.de>
2021-11-02 23:25:58 +0100

    php 7.4

--- php.sh
+++ php.sh
@@ -7,31 +7,31 @@ minimal_apt_get_install \
   make \
   php-apcu \
   php-apcu-bc \
-  php7.3 \
-  php7.3-bcmath \
-  php7.3-bz2 \
-  php7.3-cli \
-  php7.3-common \
-  php7.3-curl \
-  php7.3-dev \
-  php7.3-gd \
-  php7.3-gmp \
-  php7.3-imap \
-  php7.3-intl \
-  php7.3-json \
-  php7.3-mbstring \
-  php7.3-mysql \
-  php7.3-opcache \
-  php7.3-pgsql \
-  php7.3-pspell \
-  php7.3-readline \
-  php7.3-recode \
-  php7.3-soap \
-  php7.3-sqlite3 \
-  php7.3-xml \
-  php7.3-xmlrpc \
-  php7.3-xsl \
-  php7.3-zip \
+  php7.4 \
+  php7.4-bcmath \
+  php7.4-bz2 \
+  php7.4-cli \
+  php7.4-common \
+  php7.4-curl \
+  php7.4-dev \
+  php7.4-gd \
+  php7.4-gmp \
+  php7.4-imap \
+  php7.4-intl \
+  php7.4-json \
+  php7.4-mbstring \
+  php7.4-mysql \
+  php7.4-opcache \
+  php7.4-pgsql \
+  php7.4-pspell \
+  php7.4-readline \
+  php7.4-recode \
+  php7.4-soap \
+  php7.4-sqlite3 \
+  php7.4-xml \
+  php7.4-xmlrpc \
+  php7.4-xsl \
+  php7.4-zip \
   php-pear \
   php-redis \
   php-memcached \
@@ -41,25 +41,25 @@ minimal_apt_get_install \
   #
 
 # Enable apc on cli for unit tests
-echo "apc.enable_cli=1" >> /etc/php/7.3/mods-available/apcu.ini
-echo "apc.slam_defense=0" >> /etc/php/7.3/mods-available/apcu.ini
+echo "apc.enable_cli=1" >> /etc/php/7.4/mods-available/apcu.ini
+echo "apc.slam_defense=0" >> /etc/php/7.4/mods-available/apcu.ini
 
 # Disable opcache on php 7.1 since that triggers segfaults 'zend_mm_heap corrupted' with vfsStream 1.6.4 (currently)
-echo "opcache.enable_cli=0" >> /etc/php/7.3/cli/conf.d/10-opcache.ini
+echo "opcache.enable_cli=0" >> /etc/php/7.4/cli/conf.d/10-opcache.ini
 
 # Enable phar writing
-sed -i s/';phar.readonly = On'/'phar.readonly = Off'/ /etc/php/7.3/cli/php.ini
+sed -i s/';phar.readonly = On'/'phar.readonly = Off'/ /etc/php/7.4/cli/php.ini
 
 # Restrict cli based php.ini settings for php -S web server to have sane values in acceptance tests
-sed -i s/'memory_limit = -1'/'memory_limit = 2G'/ /etc/php/7.3/cli/php.ini
-sed -i s/'max_execution_time = 30'/'max_execution_time = 240'/ /etc/php/7.3/cli/php.ini
-sed -i s/'; max_input_vars = 1000'/'max_input_vars = 1500'/ /etc/php/7.3/cli/php.ini
+sed -i s/'memory_limit = -1'/'memory_limit = 2G'/ /etc/php/7.4/cli/php.ini
+sed -i s/'max_execution_time = 30'/'max_execution_time = 240'/ /etc/php/7.4/cli/php.ini
+sed -i s/'; max_input_vars = 1000'/'max_input_vars = 1500'/ /etc/php/7.4/cli/php.ini
 
-echo "xdebug.max_nesting_level = 400" >> /etc/php/7.3/mods-available/xdebug.ini
+echo "xdebug.max_nesting_level = 400" >> /etc/php/7.4/mods-available/xdebug.ini
 
 # mssql driver
 # pecl install sqlsrv
-# echo extension=sqlsrv.so >> /etc/php/7.3/mods-available/sqlsrv.ini
+# echo extension=sqlsrv.so >> /etc/php/7.4/mods-available/sqlsrv.ini
 # phpenmod sqlsrv
 
 # Prepare an additional php.ini file that does *NOT* include xdebug
@@ -82,6 +82,6 @@ minimal_apt_get_install \
   sqlite3 \
   #
 
-# Install composer
-curl -sSL https://getcomposer.org/download/1.10.17/composer.phar -o /usr/bin/composer
+# Install latest composer
+curl -sSL https://getcomposer.org/composer-2.phar -o /usr/bin/composer
 chmod +x /usr/bin/composer
